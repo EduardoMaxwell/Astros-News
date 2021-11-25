f678eb5e4adb4150b40ef8230b8a5910f64a2ee7
Eduardo Maxwell <eduardomaxwell2011@gmail.com>
2021-11-24 21:50:16 -0400

    added dependencies and configs for koin

---
+++ app/src/main/java/br/com/eduardomaxwell/astranovos/presentation/di/PresentationModule.kt
@@ -0,0 +1,31 @@
+package br.com.eduardomaxwell.astranovos.presentation.di
+
+import br.com.eduardomaxwell.astranovos.presentation.ui.home.HomeViewModel
+import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+// TODO 005: Criar uma função privada viewModelModule()
+// TODO 006: Criar uma função load()
+
+/*object torna nossa classe um Singleton*/
+object PresentationModule {
+
+    /*
+    *funcao que chamaremos na class App
+    * loadKoinModules recebe nossoas instancias
+    */
+    fun load() {
+        loadKoinModules(viewModelModule())
+    }
+
+    private fun viewModelModule(): Module {
+        /*aqui dentro serão instanciados
+        os objetos que passarmos . Koin tratará
+        de instancia-los pra nós
+        */
+        return module {
+            factory { HomeViewModel() }
+        }
+    }
+}
